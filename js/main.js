481cce7cee87dfa02781a5352eca63665e19f256
namitjuneja <juneja.namit@gmail.com>
2020-01-07 16:09:41 +0530

    fixed countdown time

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "January 15, 2020 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
